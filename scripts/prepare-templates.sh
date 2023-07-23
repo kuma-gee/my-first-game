48722b038893585fd21892c03c583a950f5c66aa
myin <minyin142p@gmail.com>
2023-07-23 15:27:20 +0200

    fix: fix export template location

--- scripts/prepare-templates.sh
+++ scripts/prepare-templates.sh
@@ -2,12 +2,12 @@
 
 VERSION=$1
 
-TARGET=~/.local/share/godot/templates/${VERSION}.stable
+TARGET=~/.local/share/godot/export_templates/${VERSION}.stable
 
 if [ -f "$TARGET" ]; then
     echo "Templates already installed"
     exit
 fi
 
-mkdir -v -p ~/.local/share/godot/templates
-mv /root/.local/share/godot/templates/${VERSION}.stable $TARGET
+mkdir -v -p ~/.local/share/godot/export_templates
+mv /root/.local/share/godot/export_templates/${VERSION}.stable $TARGET
